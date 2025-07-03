e8e49a8f7dfa0df6043123356751046620c95de5
Ioanna Anyfanti <ioanna@turintech.ai>
2025-07-03 10:50:13 +0300

    Add files via upload

---
+++ 3classes.py
@@ -0,0 +1,16 @@
+class FirstClass:
+    def __init__(self):
+        print("This is the First Class"
+
+class SecondClass:
+    def __init__(self):
+        print("This is the Second Class")
+        
+    def method(self):
+        return "Method in Second Class"
+
+class ThirdClass:
+    def __init__(self):
+        print("This is the Third Class")
+    def another_method(self
+        return "Method in Third Class"
